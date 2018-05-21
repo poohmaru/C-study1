a3cfb59a63aa678f056656b8d64c14811c68e920
poohmaru <marinaraibis@gmail.com>
2018-05-21 18:34:18 +0900

    add Project2 (lesson 10:array)

---
+++ Project2/Project2/Source.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	int test[5] = { 80,60,55,22,75 };
+	printf("value of test[0] is %d . \n", test[0]);
+	printf("adress of test[0] is %p . \n", &test[0]);
+	printf("value of test[0] is %p . \n", test);
+
+	return 0;
+
+
+}
+
